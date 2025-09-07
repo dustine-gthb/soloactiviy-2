87683b9e08915bcb0e1ce9af22e634f40e05bedf
Dustineeee <dustinekent@gmail.com>
2025-09-07 11:30:11 +0800

    Add files via upload

---
+++ ACT#2SOLO.cpp
@@ -0,0 +1,24 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    string fullname;
+    string cys;
+    string birthday;
+    string motto;
+    
+    cout << "Input Full name: ";
+    getline(cin, fullname);
+    cout << "Input Course, Yr. & Sec.: ";
+    getline(cin, cys);
+    cout << "Input Birthday: ";
+    getline (cin, birthday);
+    cout << "Input Motto/Motivation in Life: ";
+    getline(cin, motto);
+    
+    cout << endl << "Hi! I'm " << fullname << " of " << cys << ". Welcome to Data Structures and Algorithm! "<< endl
+    << "My Birthday is on " << birthday << ". and my motto/motivation in life is " << motto;
+}
